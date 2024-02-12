849101ebfbc29e2da6c1d93ad94ce18287c277aa
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-02-12 03:22:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const client = new Client({
 	presence: {
 		status: 'dnd',
 		activities: [{
-			type: 5,
+			type: 0,
 			name: 'Gnomeregan',
 			details: 'Located in Dun Morogh, the technological wonder known as Gnomeregan has been the gnomes capital city for generations.',
 			state: 'Killing Mekgineer Thermaplugg',
 			createdTimestamp: Date.now(),
 			assets: [{
-				largeImage: 'https://cdn.discordapp.com/attachments/426766656174555147/1206429935611150356/81140-gnomeregan.jpg?ex=65dbfa6f&is=65c9856f&hm=def8012ae2d575dd0624d5e3297c37158cf96bff7234c3b05e00831801465051',
+				largeImage: 'https://i.imgur.com/Z1Drhru.jpeg',
 				largeText: 'Gnomeregan',
 				smallImage: 'discord-icon',
 				smallText: 'Gnomeregan'
