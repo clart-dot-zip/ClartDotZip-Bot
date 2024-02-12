18cf297144b1fc4cf7ee733edfe363f1d83eed1d
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-02-12 04:13:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const client = new Client({
 			state: 'Killing Mekgineer Thermaplugg',
 			createdTimestamp: Date.now(),
 			assets: [{
-				large_image: 'https://cdn.discordapp.com/app-assets/1206385637603938314/1206441092895998013.png',
+				large_image: '1206441092895998013',
 				largeText: 'Gnomeregan',
 				smallImage: 'discord-icon',
 				smallText: 'Gnomeregan'
