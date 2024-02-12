2e048717e061ffcc132e8524927cdbc030ad29f1
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-02-12 04:16:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const client = new Client({
 			assets: {
 				large_image: 'https://cdn.discordapp.com/app-assets/1206385637603938314/1206441092895998013.png',
 				largeText: 'Gnomeregan',
-				smallImage: 'https://cdn.discordapp.com/app-assets/1206385637603938314/1206441092895998013.png',
+				small_image: 'https://cdn.discordapp.com/app-assets/1206385637603938314/1206441092895998013.png',
 				smallText: 'Gnomeregan'
 			}
 		}],
