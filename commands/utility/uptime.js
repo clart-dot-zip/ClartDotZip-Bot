c4028d91932df12fb6b5d5d2c49cf15bab0548e9
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-02-16 20:14:52 +0000

    Update uptime.js

--- commands/utility/uptime.js
+++ commands/utility/uptime.js
@@ -1,20 +1,20 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { client } = require('discord.js');
 
-let totalSeconds = (client.uptime / 1000);
-let days = Math.floor(totalSeconds / 86400);
-totalSeconds %= 86400;
-let hours = Math.floor(totalSeconds / 3600);
-totalSeconds %= 3600;
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = Math.floor(totalSeconds % 60);
-
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('uptime')
 		.setDescription('Responds with bot uptime'),
 	async execute(interaction) {
+        
+        let totalSeconds = (client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        totalSeconds %= 86400;
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
+
 		await interaction.reply(`I've been running for ${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds.`);
 	},
 };
