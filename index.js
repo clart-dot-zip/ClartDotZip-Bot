7aa73b89b7503184147bebb4117127c184418783
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-02-16 20:12:27 +0000

    registering command

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits} = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits} = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -29,6 +31,25 @@ const client = new Client({
 	}
 });
 
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
@@ -42,13 +63,3 @@ client.login(token);
 client.on('ready', async () => {
 	console.log(`Activity ${JSON.stringify(client.user.presence)}`)
 })
-
-client.on("messageCreate", (message) => {
-    if (message.author.bot) return false;
-
-    if (message.content.includes("@here") || message.content.includes("@everyone") || message.type == "REPLY") return false;
-
-    if (message.mentions.has(client.user.id)) {
-        message.channel.send("Hello there!");
-    }
-});
