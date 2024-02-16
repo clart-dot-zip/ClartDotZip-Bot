45c66de464f175a04f8c0078e1395cee3b5e0a22
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-02-16 20:19:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ for (const folder of commandFolders) {
 		const command = require(filePath);
 		if('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
+			console.log(`[LOG] The command ${command.data.name} has been registered.`);
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
@@ -63,3 +64,25 @@ client.login(token);
 client.on('ready', async () => {
 	console.log(`Activity ${JSON.stringify(client.user.presence)}`)
 })
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
