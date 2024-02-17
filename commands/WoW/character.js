89695bc57bbdb4cf0e46477e14aff15358d8401a
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-02-17 03:03:15 +0000

    Update character.js

--- commands/WoW/character.js
+++ commands/WoW/character.js
@@ -5,7 +5,14 @@ const { wowClientId, wowSecret } = require('../../config.json');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('character')
-		.setDescription('Lists information regarding a WoW character.'),
+		.setDescription('Lists information regarding a WoW character.')
+        .addStringOption(option =>
+            option.setName('charactername')
+                .setDescription('Name of the character to search.'))
+        .addStringOption(option =>
+            option.setName('realm')
+                .setDescription('Name of the realm the character is on.')),
+
 	async execute(interaction) {
 
         const api = new BlizzAPI({
@@ -13,9 +20,14 @@ module.exports = {
             clientId: wowClientId,
             clientSecret: wowSecret,
         })
-
-        const data = await api.query("/profile/wow/character/argent-dawn/broccocoli?namespace=profile-eu");
-        console.log(data);
+        try {
+            const data = await api.query("/profile/wow/character/argent-dawn/broccocoli?namespace=profile-eu");
+          } catch (error) {
+            console.error(error);
+            // Expected output: ReferenceError: nonExistentFunction is not defined
+            // (Note: the exact output may be browser-dependent)
+          }
+          
 		await interaction.reply(`uh oh`);
 	},
 };
