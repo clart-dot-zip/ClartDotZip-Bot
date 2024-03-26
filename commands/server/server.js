b829e8492918545acbec56ad1e25a2b2314417d8
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-03-26 23:48:12 +0000

    Update server.js

--- commands/server/server.js
+++ commands/server/server.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const Nodeactyl = require('nodeactyl');
 const config = require('../../config/config.json');
 const application = new Nodeactyl.NodeactylApplication(config.panelAddress, config.pteroApi);
+const util = require('util');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,8 +11,8 @@ module.exports = {
 	async execute(interaction) {
         application.getAllServers().then((response) => {
             for (var i = 0; i < response.meta.pagination.count; i++) {
-                server = JSON.parse(response.data[i].attributes);
-                console.log(server);
+                server = response.data[i].attributes;
+                console.log(util.inspect(server, {depth: null}));
             }
             interaction.reply('Server list: ' + response);
         }).catch((error) => {  
