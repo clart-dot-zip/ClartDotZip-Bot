a8f46091f403b47ced38a07fa4291148a588e62e
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-03-26 23:25:12 +0000

    Create server.js

---
+++ commands/server/server.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Nodeactyl = require('nodeactyl');
+const config = require('../../config.json');
+const application = new Nodeactyl.NodeactylApplication(config.panelAddress, config.pteroApi);
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('server')
+		.setDescription('list server'),
+	async execute(interaction) {
+        let temp = application.getAllServers();
+        await temp;
+        console.log(temp);
+	},
+};
