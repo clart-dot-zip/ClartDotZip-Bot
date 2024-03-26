49f4dfe86fcac462c219f21b72d7f3b8b48cee74
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-03-26 23:27:04 +0000

    Update server.js

--- commands/server/server.js
+++ commands/server/server.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const Nodeactyl = require('nodeactyl');
-const config = require('../../config.json');
+const config = require('../../config/config.json');
 const application = new Nodeactyl.NodeactylApplication(config.panelAddress, config.pteroApi);
 
 module.exports = {
