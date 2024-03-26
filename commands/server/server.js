12d17e65fff18549a00549741ad63f1c9578621b
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-03-26 23:35:16 +0000

    Update server.js

--- commands/server/server.js
+++ commands/server/server.js
@@ -10,8 +10,6 @@ module.exports = {
 	async execute(interaction) {
         let temp = application.getAllServers();
         await temp;
-        temp.data.forEach(item => { 
-            console.log(item);
-        })
+        console.log(temp.data);
 	},
 };
