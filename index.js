6d91e4dd99671c2c91b1fb25f4f110eebc6ef5cf
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-03-27 00:15:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits} = require('discord.js');
 const { token } = require('./config/config.json');
+const cron = require('node-cron');
 
 // Create a new client instance
 const client = new Client({ 
