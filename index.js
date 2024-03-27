cb7c8c0b5e76a7833f8beb20672f5ae645ac8fee
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-03-27 03:52:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ cron.schedule('*/5 * * * * *', async () => {
 			const serverData = server.attributes;
 			const identifier = serverData.identifier;
 			const name = serverData.name; // Extract name attribute
+			const description = serverData.description;
 
 			// Fetch server status asynchronously
 			const status = await getServerStatus(identifier);
