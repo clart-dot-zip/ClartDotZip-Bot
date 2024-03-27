9466b7bf541172bb0a98323ca24561a1199f8a7d
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-03-27 03:27:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ cron.schedule('*/5 * * * * *', async () => {
                 const status = await getServerStatus(identifier);
 				const details = await getServerDetails(identifier);
 
-				console.log(details.relationships.allocations.data.attributes);
+				console.log(details.relationships.allocations.data);
 
                 // Push server data with status to array
                 serverDataWithStatus.push({
