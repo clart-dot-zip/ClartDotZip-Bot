9c63ad4ec1242360ba06fe32e59e12d832498d78
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-03-28 16:41:20 +0000

    Update cronJob.js

--- cronJob.js
+++ cronJob.js
@@ -12,7 +12,7 @@ const getServerStatus = (identifier) => clientApp.getServerStatus(identifier);
 const getServerDetails = (identifier) => clientApp.getServerDetails(identifier);
 
 const serverDataWithStatus = [];
-const currentCache = [];
+let currentCache = [];
 
 async function isImgUrl(url) {
     return fetch(url, {method: 'HEAD'}).then(res => {
