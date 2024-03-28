5af8296ffb7ad2945395f77990ba84fb55449bc1
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-03-28 15:07:15 +0000

    Update cronJob.js

--- cronJob.js
+++ cronJob.js
@@ -35,9 +35,10 @@ async function updateServerData(client) {
                 const serverData = server.attributes;
                 const identifier = serverData.identifier;
 
+                console.log(server);
+
                 if (currentServerData[identifier] == serverData[identifier]) { continue; }
 
-                console.log('Fetching server data for:', identifier)
                 const name = serverData.name; // Extract name attribute
                 var description = serverData.description;
                 var thumbnail =  (await isImgUrl("https://clart.zip/resources/" + identifier + ".png")) ? "https://clart.zip/resources/" + identifier + ".png" : "https://clart.zip/resources/default.png";
