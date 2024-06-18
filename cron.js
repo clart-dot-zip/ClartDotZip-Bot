787dc58faf426bf06319d2cdea93c45ff1c11896
Jordan Bickle <jh016723@student.reading.ac.uk>
2024-06-18 19:09:13 +0100

    Update cron.js

--- cron.js
+++ cron.js
@@ -70,103 +70,111 @@ async function cron(discord){
 }
 
 async function update_servers(){
-    const response = await getAllServers()
-    let servers
+    try {
+        const response = await getAllServers()
+        let servers
 
-    if (response.meta.pagination.total_pages === 1){
-        servers = response.data
-    } else {
-        servers = []
-        for (let page = 2; page < reponse.meta.pagination.total_pages; page++){
-            servers.push(
-                getServerPage(page)
-                    .then((returnedPage) => returnedPage)
-            )
-        }
-        servers.splice(1, 0, Promise.resolve(response.data))
-        servers = await Promise.all(servers)
-        servers = servers.flat(1)
-    }
-
-    const details = await Promise.all(servers.map((server) => new Promise(async function (res, rej){
-        const id = server.attributes.identifier
-        let out = [id]
-        let wait = [getServerStatus(id), getServerDetails(id)]
-        wait = await Promise.all(wait)
-
-        let allocation = wait[1].relationships.allocations.data.find(allocation => allocation.attributes.is_default)
-        let ip = null
-        if (allocation !== undefined){
-            ip = `${allocation.attributes.ip_alias}:${allocation.attributes.port}`
+        if (response.meta.pagination.total_pages === 1){
+            servers = response.data
+        } else {
+            servers = []
+            for (let page = 2; page < reponse.meta.pagination.total_pages; page++){
+                servers.push(
+                    getServerPage(page)
+                        .then((returnedPage) => returnedPage)
+                )
+            }
+            servers.splice(1, 0, Promise.resolve(response.data))
+            servers = await Promise.all(servers)
+            servers = servers.flat(1)
         }
 
-        out.push({
-            'id': wait[1].internal_id,
-            'online_status': wait[0],
-            'address': ip,
-            'identifier': id,
-            'name': wait[1].name,
-            'description': wait[1].description === "" ? "N/A" : wait[1].description,
-            'thumbnail': `https://clart.zip/resources/${id}.png`,
-        })
-        return res(out)
-    })))
+        const details = await Promise.all(servers.map((server) => new Promise(async function (res, rej){
+            const id = server.attributes.identifier
+            let out = [id]
+            let wait = [getServerStatus(id), getServerDetails(id)]
+            wait = await Promise.all(wait)
 
-    let ink = cache.keys()
-    let outk = new Set(details.map(([id, _]) => id))
+            let allocation = wait[1].relationships.allocations.data.find(allocation => allocation.attributes.is_default)
+            let ip = null
+            if (allocation !== undefined){
+                ip = `${allocation.attributes.ip_alias}:${allocation.attributes.port}`
+            }
 
-    for (const inKey of ink){
-        if (!outk.has(inKey)){
-            cache.delete(inKey)
+            out.push({
+                'id': wait[1].internal_id,
+                'online_status': wait[0],
+                'address': ip,
+                'identifier': id,
+                'name': wait[1].name,
+                'description': wait[1].description === "" ? "N/A" : wait[1].description,
+                'thumbnail': `https://clart.zip/resources/${id}.png`,
+            })
+            return res(out)
+        })))
+
+        let ink = cache.keys()
+        let outk = new Set(details.map(([id, _]) => id))
+
+        for (const inKey of ink){
+            if (!outk.has(inKey)){
+                cache.delete(inKey)
+            }
         }
-    }
 
-    for (const [id, detail] of details){
-        cache.set(id, detail)
+        for (const [id, detail] of details){
+            cache.set(id, detail)
+        }
+    } catch (e){
+        consoleLog(2, "An error occurred in update_servers:", e)
     }
 }
 
 async function update_messages(discord){
-    let changed = false
+    try {
+        let changed = false
 
-    for (const [key, messageData] of messages.entries()){
-        const serverData = cache.get(key)
+        for (const [key, messageData] of messages.entries()){
+            const serverData = cache.get(key)
 
-        if (serverData === undefined){
-            let guild = await discord.guilds.fetch(messageData.guild_id)
-            let channel = await guild.channels.fetch(messageData.channel_id)
-            let message = await channel.messages.fetch(messageData.message_id)
-            await message.delete()
-            messages.delete(key)
-            changed = true
-            consoleLog(1, ` Deleted ${messageData.name} server message, server no longer exists.`)
-        } else if (
-            messageData.online_status !== serverData.online_status ||
-            messageData.address !== serverData.address ||
-            messageData.identifier !== serverData.identifier ||
-            messageData.name !== serverData.name ||
-            messageData.description !== serverData.description ||
-            messageData.thumbnail !== serverData.thumbnail
-        ){
-            let guild = await discord.guilds.fetch(messageData.guild_id)
-            let channel = await guild.channels.fetch(messageData.channel_id)
-            let message = await channel.messages.fetch(messageData.message_id)
-            await message.edit({embeds: [buildEmbed(serverData)]})
+            if (serverData === undefined){
+                let guild = await discord.guilds.fetch(messageData.guild_id)
+                let channel = await guild.channels.fetch(messageData.channel_id)
+                let message = await channel.messages.fetch(messageData.message_id)
+                await message.delete()
+                messages.delete(key)
+                changed = true
+                consoleLog(1, ` Deleted ${messageData.name} server message, server no longer exists.`)
+            } else if (
+                messageData.online_status !== serverData.online_status ||
+                messageData.address !== serverData.address ||
+                messageData.identifier !== serverData.identifier ||
+                messageData.name !== serverData.name ||
+                messageData.description !== serverData.description ||
+                messageData.thumbnail !== serverData.thumbnail
+            ){
+                let guild = await discord.guilds.fetch(messageData.guild_id)
+                let channel = await guild.channels.fetch(messageData.channel_id)
+                let message = await channel.messages.fetch(messageData.message_id)
+                await message.edit({embeds: [buildEmbed(serverData)]})
 
-            messageData.online_status = serverData.online_status
-            messageData.address = serverData.address
-            messageData.identifier = serverData.identifier
-            messageData.name = serverData.name
-            messageData.description = serverData.description
-            messageData.thumbnail = serverData.thumbnail
-            messages.set(key, messageData)
-            changed = true
-            consoleLog(1, ` Updated ${serverData.name}`)
+                messageData.online_status = serverData.online_status
+                messageData.address = serverData.address
+                messageData.identifier = serverData.identifier
+                messageData.name = serverData.name
+                messageData.description = serverData.description
+                messageData.thumbnail = serverData.thumbnail
+                messages.set(key, messageData)
+                changed = true
+                consoleLog(1, ` Updated ${serverData.name}`)
+            }
         }
-    }
 
-    if (changed){
-        await messages.save()
+        if (changed){
+            await messages.save()
+        }
+    } catch (e){
+        consoleLog(2, "An error occurred in update_messages:", e)
     }
 }
 
